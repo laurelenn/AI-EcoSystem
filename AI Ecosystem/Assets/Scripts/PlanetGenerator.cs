c24ec7d7c77558631fda3914c7072e202e9b9882
Mathilde <mathildedumoulin54@gmail.com>
2021-02-05 14:25:37 +0100

    :construction: [Scripts] Add UI Scripts

---
+++ AI Ecosystem/Assets/Scripts/PlanetGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetGenerator : MonoBehaviour
+{
+     public Mesh objectToCreate;
+     public GameObject sun;
+     public Material grass;
+     public Material lava;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void generatePlanet(string planetName, int planetSize, int planetDistance){
+
+        //Setting name
+        Debug.Log("Generate " + planetName);
+        var gameObject = new GameObject(planetName);
+
+        //Child from sun 
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        meshFilter.sharedMesh = objectToCreate;
+        gameObject.transform.parent = sun.transform;
+        
+        
+        //Distance and scale
+        //gameObject.transform.position = new Vector3(0,0,-100);
+        gameObject.transform.position = new Vector3(0,0,-planetDistance);
+        gameObject.transform.localScale = new Vector3(1,1,1);
+        gameObject.transform.localScale = new Vector3(planetSize/100f,planetSize/100f, planetSize/100f);
+
+        gameObject.AddComponent<PlanetController>();
+        gameObject.GetComponent<PlanetController>().speed = 30;
+        
+        
+        MeshRenderer mesh = gameObject.GetComponent<MeshRenderer>();
+        mesh.material = Resources.Load("planet", typeof(Material)) as Material;
+
+    }
+}
---
+++ AI Ecosystem/Assets/Scripts/UI/ButtonHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickGenerate(){
+        //appeler l'autre script
+        Debug.Log("On appelle l'autre script");
+        string planetName = GameObject.Find("PlanetNameText").GetComponent<UnityEngine.UI.Text>().text;
+        if(planetName != ""){
+            int planetSize = (int)GameObject.Find("sizeSlider").GetComponent<Slider>().value;
+            int planetDistance = (int)GameObject.Find("DistanceSunSlider").GetComponent<Slider>().value;
+            GameObject.Find("Manager").GetComponent<PlanetGenerator>().generatePlanet(planetName, planetSize, planetDistance);
+        }else{
+            Debug.Log("Planet name is empty !");
+        }
+        
+    }
+}
