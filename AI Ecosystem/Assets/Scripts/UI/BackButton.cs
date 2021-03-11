9a7017c4d9f832eac153756c5058ca83f6aa2c63
Mathilde <mathildedumoulin54@gmail.com>
2021-03-11 23:12:31 +0100

    :package: Adding Scripts (no connections)

---
+++ AI Ecosystem/Assets/Scripts/UI/BackButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+
+    public Camera cam1;
+    public Camera cam2;
+    Vector3 initialPos;
+   
+    void Start()
+    {
+        initialPos = gameObject.transform.position;
+    }
+    void Update()
+    {
+        if (cam1.enabled)
+        {
+            gameObject.transform.position = new Vector3(initialPos.x, initialPos.y-8000, initialPos.z);
+        }
+        else
+        {
+            gameObject.transform.position = initialPos;
+        }
+       
+    }
+
+    
+    public void goBackToSolarSystem()
+    {
+        if (cam2.enabled && !cam1.enabled)
+        {
+            cam2.enabled = !cam2.enabled;
+            cam1.enabled = !cam1.enabled;
+            gameObject.transform.position = new Vector3(initialPos.x, initialPos.y - 8000, initialPos.z);
+        }
+        
+        
+    }
+}
---
+++ AI Ecosystem/Assets/Scripts/UI/SwitchButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//When button is clicked, switches camera display to the right planet
+public class SwitchButton : MonoBehaviour
+{
+    public Camera cam;
+    public Camera cam2;
+    public int idButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        cam2 = GameObject.Find("TerrainCamera").GetComponent<Camera>();
+
+        cam2.enabled = false;
+        cam.enabled = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void switchDisplay()
+    {
+        if (!cam2.enabled)
+        {
+            cam.enabled = !cam.enabled;
+            cam2.enabled = !cam2.enabled;
+        }
+       
+        updateCameraPos();
+       
+    }
+
+    public void updateCameraPos()
+    {
+        GameObject matchingTerrain;
+        GameObject[] terrains = GameObject.FindGameObjectsWithTag("Closeup");
+        foreach(GameObject go in terrains)
+        {
+            if (go.GetComponent<TerrainGround>().idTerrain == idButton)
+            {
+                matchingTerrain = go;
+                cam2.gameObject.transform.position = matchingTerrain.transform.Find("CameraSpawn").transform.position;
+                cam2.gameObject.transform.LookAt(matchingTerrain.transform.Find("LookingPoint"));
+            }
+        }           
+    }
+    
+}
