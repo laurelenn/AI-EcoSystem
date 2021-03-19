2bff054c542f8285f8d4d65424f4ca898de1f721
laurelenn <laurelenn-sangare@laposte.net>
2021-03-19 13:57:20 +0100

    Avocado Builder finish + Add Prefab in scene
    - Need to add meterials

--- AI Ecosystem/Assets/Scripts/Genetic_algorithm/AvocadoBuilder.cs
+++ AI Ecosystem/Assets/Scripts/Genetic_algorithm/AvocadoBuilder.cs
@@ -7,32 +7,67 @@ public class AvocadoBuilder : MonoBehaviour {
     [SerializeField] GameObject leaves;
 
     void Start(){
-
+        buildAvocado(new Vector3(-2050, 0, 110),100,300); // 10
+        buildAvocado(42,120); // 5 ranges // 6 volumes
     }
 
     void Update(){
 
     }
 
-    public void buildAvocado(int height, int volume){
+    public void buildAvocado(int height, int volume){ // Position = (0,0,0)
         // Height : 0_127
         // Volume : 0_511
 
-        int heightAvocado =(int)((height+10)/10) ; // Number of ranges - Between 1_12
+        int heightAvocado =(int)((height+10)/10); // Number of ranges - Between 1_12
         int volumeAvocado = (int)((volume+100)/100); // Between 1 and 6 leaves by range
-        int angle = (int)(360-(360/volumeAvocado)/volumeAvocado);
+        int angle = (int)(360/volumeAvocado);
 
-        //Create gameObject "Avocado_Plant"
-        for (int i=0; i<heightAvocado; i++){ // Each Range + 1.5y // scale X, Y = 50+50*(heightAvocado-i)
-        //Create gameObject Range_i
+        GameObject plant = new GameObject("Avocado_Plant");
+        //plant.transform.parent = terrainX?transform; --> Passer gameobject en paramètre de la fonction si besoin.
+
+        for (int i=0; i<heightAvocado; i++){ // Each Range + 1.5y
+            GameObject range = new GameObject("Range_"+i);
+            range.transform.parent=plant.transform;
+            range.transform.position = new Vector3 (0, 1.5f*i, 0);
+
             for (int j=0; j<volumeAvocado; j++){ // Each Leaves YRotation = 0+angle - Child of Range_i
-                //Instantiate leaves
+                Quaternion angleLeaf = Quaternion.Euler(-90, angle*j, 0);
+                GameObject leaf = Instantiate(leaves, range.transform.position, angleLeaf, range.transform);
+                leaf.transform.name = "Leaf_"+j;
+                int scaleLeaf = 50+50*(heightAvocado-i);
+                leaf.transform.localScale = new Vector3(scaleLeaf,scaleLeaf,150);
+                leaf.SetActive(true);
             }
+            int rotate = Random.Range(10, 170);
+            range.transform.rotation = Quaternion.Euler(0, rotate, 0);
         }
     }
 
+    public void buildAvocado(Vector3 positionPlant, int height, int volume){
+        int heightAvocado =(int)((height+10)/10);
+        int volumeAvocado = (int)((volume+100)/100); 
+        int angle = (int)(360/volumeAvocado);
 
-    public void buildAvocado(Vector3 position, int height, int volume){
+        GameObject plant = new GameObject("Avocado_Plant");
+        plant.transform.position = positionPlant;
+        //plant.transform.parent = terrainX.transform; --> Passer gameobject en paramètre de la fonction si besoin.
 
+        for (int i=0; i<heightAvocado; i++){
+            GameObject range = new GameObject("Range_"+i);
+            range.transform.parent=plant.transform;
+            range.transform.position = new Vector3 (0, 1.5f*i, 0);
+
+            for (int j=0; j<volumeAvocado; j++){
+                Quaternion angleLeaf = Quaternion.Euler(-90, angle*j, 0);
+                GameObject leaf = Instantiate(leaves, range.transform.position, angleLeaf, range.transform);
+                leaf.transform.name = "Leaf_"+j;
+                int scaleLeaf = 50+50*(heightAvocado-i);
+                leaf.transform.localScale = new Vector3(scaleLeaf,scaleLeaf,150);
+                leaf.SetActive(true);
+            }
+            int rotate = Random.Range(10, 170);
+            range.transform.rotation = Quaternion.Euler(0, rotate, 0);
+        }
     }
 }
