92af13e9dd176f84b20febedbf9627deb8f33f11
Solène <45456425+SolHaine@users.noreply.github.com>
2021-03-25 21:16:08 +0100

    Add pines + adn correction

--- AI Ecosystem/Assets/Scripts/Genetic_algorithm/PinesBuilder.cs
+++ AI Ecosystem/Assets/Scripts/Genetic_algorithm/PinesBuilder.cs
@@ -7,20 +7,20 @@ public class PinesBuilder : MonoBehaviour
     [SerializeField] GameObject pineLeaves;
 
 
-    public GameObject buildPine(int height, int volume)
+    public GameObject buildPine(int height, int volume, GameObject terrainX)
     { // Position = (0,0,0)
-        return buildPine(new Vector3(0, 0, 0), height, volume);
+        return buildPine(new Vector3(0, 0, 0), height, volume, terrainX);
     }
 
-    public GameObject buildPine(Vector3 positionPlant, int height, int volume)
+    public GameObject buildPine(Vector3 positionPlant, int height, int volume, GameObject terrainX)
     {
         int heightPine = (int)((height + 10) / 7);
         int volumePine = (int)((volume + 100) / 100);
         int angle = (int)(360 / (volumePine + 1));
 
         GameObject plant = new GameObject("Conifer_Plant");
-        plant.transform.position = positionPlant;
-
+        // plant.transform.position = positionPlant;
+        plant.transform.parent = terrainX.transform; // --> Passer gameobject en paramètre de la fonction si besoin.
         for (int i = 0; i < heightPine; i++)
         {
             //position in height
@@ -33,7 +33,7 @@ public class PinesBuilder : MonoBehaviour
             leaf.transform.localScale = new Vector3(scaleLeaf, scaleLeaf, 150);
             leaf.SetActive(true);
         }
-
+        plant.transform.localPosition = positionPlant;
         return plant;
     }
 }
